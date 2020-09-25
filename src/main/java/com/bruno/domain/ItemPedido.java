bdf7ae4449ff741f1c5f0839d76aa23430b1e344
Bruno Felisbino <bruno_felisbino@hotmail.com>
2020-09-25 20:32:07 -0300

    Inserindo Pedido

--- src/main/java/com/bruno/domain/ItemPedido.java
+++ src/main/java/com/bruno/domain/ItemPedido.java
@@ -1,7 +1,6 @@
 package com.bruno.domain;
 
 import java.io.Serializable;
-
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
@@ -9,12 +8,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ItemPedido implements Serializable {
-    
-
     private static final long serialVersionUID = 1L;
+
     @JsonIgnore
     @EmbeddedId
-private ItemPedidoPK id = new ItemPedidoPK();
+    private ItemPedidoPK id = new ItemPedidoPK();
+
     private Double desconto;
     private Integer quantidade;
     private Double preco;
@@ -22,16 +21,34 @@ private ItemPedidoPK id = new ItemPedidoPK();
     public ItemPedido() {
     }
 
-public double getSubTotal() {
-    return (preco - desconto) * quantidade ;
-}
-
-    public ItemPedido(Pedido pedido , Produto produto, Double desconto, Integer quantidade, Double preco) {
-       id.setPedido(pedido);
-       id.setProduto(produto);
+    public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
+        super();
+        id.setPedido(pedido);
+        id.setProduto(produto);
         this.desconto = desconto;
         this.quantidade = quantidade;
         this.preco = preco;
+    }
+
+    public double getSubTotal() {
+        return (preco - desconto) * quantidade;
+    }
+
+    @JsonIgnore
+    public Pedido getPedido() {
+        return id.getPedido();
+    }
+
+    public void setPedido(Pedido pedido) {
+        id.setPedido(pedido);
+    }
+
+    public Produto getProduto() {
+        return id.getProduto();
+    }
+
+    public void setProduto(Produto produto) {
+        id.setProduto(produto);
     }
 
     public ItemPedidoPK getId() {
@@ -65,38 +82,30 @@ public double getSubTotal() {
     public void setPreco(Double preco) {
         this.preco = preco;
     }
-    @JsonIgnore
-   public Pedido getPedido () {
-       return id.getPedido();
-   } 
-   
-   public Produto getProduto () {
-       return id.getProduto();
-   }
 
-   @Override
-   public int hashCode() {
-       final int prime = 31;
-       int result = 1;
-       result = prime * result + ((id == null) ? 0 : id.hashCode());
-       return result;
-   }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-   @Override
-   public boolean equals(Object obj) {
-       if (this == obj)
-           return true;
-       if (obj == null)
-           return false;
-       if (getClass() != obj.getClass())
-           return false;
-       ItemPedido other = (ItemPedido) obj;
-       if (id == null) {
-           if (other.id != null)
-               return false;
-       } else if (!id.equals(other.id))
-           return false;
-       return true;
-   }
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemPedido other = (ItemPedido) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
--- src/main/java/com/bruno/service/PedidoService.java
+++ src/main/java/com/bruno/service/PedidoService.java
@@ -1,8 +1,14 @@
 package com.bruno.service;
 
+import java.util.Date;
 import java.util.Optional;
 
+import com.bruno.domain.ItemPedido;
+import com.bruno.domain.PagamentoComBoleto;
 import com.bruno.domain.Pedido;
+import com.bruno.domain.enums.EstadoPagamento;
+import com.bruno.repositories.ItemPedidoRepository;
+import com.bruno.repositories.PagamentoRepository;
 import com.bruno.repositories.PedidoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +20,18 @@ import exception.ObjectNotFoundException;
 public class PedidoService {
 
     @Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+    private ProdutoService produtoService;
+    
+    @Autowired
     private PedidoRepository repo;
 
     public Pedido find(Integer id) {
@@ -23,4 +41,23 @@ public class PedidoService {
        } 
        
    
-}
+       public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
+}	
