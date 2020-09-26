1730c2b200ea07ca38124a4ec432ed315dae0a53
Bruno Felisbino <bruno_felisbino@hotmail.com>
2020-09-25 21:19:23 -0300

    Create Profile Test

---
+++ src/main/java/com/bruno/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.bruno.config;
+
+
+import java.text.ParseException;
+
+import com.bruno.service.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
---
+++ src/main/java/com/bruno/service/BCryptPasswordEncoder.java
@@ -0,0 +1,5 @@
+package com.bruno.service;
+
+public class BCryptPasswordEncoder {
+
+}
